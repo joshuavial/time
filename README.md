c3f9446e12220f4bd069f306a17a7e6896ff575c
Joshua Vial <joshua@enspiral.com>
2021-06-16 22:11:16 +1200

    adding README, tweaking config and output

---
+++ README.md
@@ -0,0 +1,9 @@
+# Time
+
+This is a simple script to help you get perspective. It will calculate how many days, weeks and months you have lived. While each monday can seem the same, you will only live your 9973rd day once.
+
+It can also show you how many hours, days, weeks, months you have left before you retire.
+
+## config
+
+.env.sample has the configuration ```node index.js``` to run.
--- index.js
+++ index.js
@@ -7,33 +7,26 @@ let birth = new Date(year, Number(month) - 1 , day)
 dateFor(birth)
 
 function dateFor(birth) {
-  let now = new Date()
-  printTally(birth, now)
-  console.log('***')
-
-  let daysTarget = 70 * 365.25
-  let retirement = add(birth, {days: daysTarget})
-  printLeft(now, retirement)
+  const now = new Date()
+  const daysTarget = process.env.RETIREMENT_AGE * 365.25
+  const retirement = add(birth, {days: daysTarget})
+  printTally(birth, now, retirement)
 }
 
-function printTally(birth, now) {
-  console.log(format(birth, 'd MMM yyyy'))
+function printTally(birth, now, retirement) {
+  const hoursPerDay = process.env.HOURS_PER_YEAR_WORKED / 365.25
+  const daysLeft = differenceInDays(retirement, now)
+  const weeksLeft = differenceInWeeks(retirement, now)
+  const monthsLeft = differenceInMonths(retirement, now)
+  const hoursLeft = parseInt(daysLeft * hoursPerDay)
+
+  console.log(format(birth, 'd MMM yyyy'), '->', format(retirement, 'd MMM yyyy'), '(if you are lucky)')
   console.log(differenceInDays(now, birth), 'days')
   console.log(differenceInWeeks(now, birth), 'weeks')
   console.log(differenceInMonths(now, birth), 'months')
+  console.log(hoursLeft, 'hours left')
+  console.log(daysLeft, 'days left')
+  console.log(weeksLeft, 'weeks left')
+  console.log(monthsLeft, 'months left')
 }
 
-function printLeft(now, retirement) {
-  let hoursPerDay = 2000 / 365.25
-  let daysDiff = differenceInDays(retirement, now)
-  let weeksDiff = differenceInWeeks(retirement, now)
-  let monthsDiff = differenceInMonths(retirement, now)
-
-  console.log('retirement: ', format(retirement, 'd MMM yyyy'))
-  console.log(`\
-${parseInt(daysDiff * hoursPerDay)} hours left \n\
-${daysDiff} days left \n\
-${weeksDiff} weeks left \n\
-${monthsDiff} months left`)
-
-}
